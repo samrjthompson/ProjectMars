d3e98a90f92fc64ba73146d769e97f098665034d
joeyisplaying <joeyisplaying@users.noreply.github.com>
2021-03-11 21:32:02 +0000

    Added cpp files

---
+++ Source/ProjectMars/Population/PopulationBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PopulationBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARS_API UPopulationBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
