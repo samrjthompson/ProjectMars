8b02a336c6904c96d4ce812a64add1fcc1d6f9e6
joeyisplaying <joeyisplaying@users.noreply.github.com>
2021-03-14 22:55:09 +0000

    Stage

---
+++ Source/ProjectMars/UI/Widgets/EconomyWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EconomyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARS_API UEconomyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
