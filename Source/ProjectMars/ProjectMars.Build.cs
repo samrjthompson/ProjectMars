fea7383c4717774e14174c754217aceea345bdb8
Sam Thompson <samthompson73@live.co.uk>
2023-10-21 17:42:15 +0100

    PM-6: Add unit tests

--- Source/ProjectMars/ProjectMars.Build.cs
+++ Source/ProjectMars/ProjectMars.Build.cs
@@ -10,7 +10,7 @@ public class ProjectMars : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "UnrealEd" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AITestSuite", "AITestSuite" });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/ProjectMars/Test/Economy/IncomeCalculatorTest.cpp
@@ -0,0 +1,45 @@
+﻿#include "Misc/AutomationTest.h"
+#include "ProjectMars/Economy/EconomyManager.h"
+#include "ProjectMars/Economy/IncomeCalculator.h"
+
+UIncomeCalculator* IncomeCalculator = NewObject<UIncomeCalculator>();
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestCalculateNetIncome, "Test net income is calculated correctly", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FTestCalculateNetIncome::RunTest(const FString& Parameters)
+{
+	{
+		// given
+		constexpr int32 GrossIncome = 500;
+		constexpr int32 GrossOutgoings = 250;
+		constexpr int32 Expected = 250;
+
+		// when
+		const int32 Actual = IncomeCalculator->CalculateNetIncome(GrossIncome, GrossOutgoings);
+
+		// then
+		TestEqual(TEXT("Test net income is calculated correctly"), Actual, Expected);
+	}
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestCalculateGrossIncome, "Test gross income is calculated correctly", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FTestCalculateGrossIncome::RunTest(const FString& Parameters)
+{
+	{
+		// given
+		TMap<EIncomeSourceType, int32> MapOfIncomeSources;
+		MapOfIncomeSources.Add(EIncomeSourceType::TaxIncome, 200);
+		MapOfIncomeSources.Add(EIncomeSourceType::TradeIncome, 350);
+		constexpr int32 Expected = 200 + 350;
+
+		// when
+		const int32 Actual = IncomeCalculator->CalculateGrossIncome(MapOfIncomeSources);
+
+		// then
+		TestEqual(TEXT("Test gross income is calculated correctly"), Actual, Expected);
+	}
+	return true;
+}
+
+
