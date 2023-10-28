ca4d31a5813d887dcc136495838b0719b4844e35
Sam Thompson <66477261+samrjthompson@users.noreply.github.com>
2023-10-28 16:04:55 +0100

    PM-41: Add Json serializer and deserializer (#32)

--- Source/ProjectMars/ProjectMars.Build.cs
+++ Source/ProjectMars/ProjectMars.Build.cs
@@ -8,7 +8,7 @@ public class ProjectMars : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "UnrealEd" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "UnrealEd", "Json" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "AITestSuite", "AITestSuite" });
 
---
+++ Source/ProjectMars/Utils/ReadWriteJsonFile.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ReadWriteJsonFile.generated.h"
+
+class FJsonObject;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARS_API UReadWriteJsonFile : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static FString ReadStringFromFile(FString FilePath, bool& bOutSuccess, FString& OutInfoMessage);
+
+	static void WriteStringToFile(FString FilePath, FString String, bool& bOutSuccess, FString& OutInfoMessage);
+	
+	static TSharedPtr<FJsonObject> ReadJson(FString JsonFilePath, bool& bOutSuccess, FString& OutInfoMessage);
+
+	static void WriteJson(FString JsonFilePath, TSharedPtr<FJsonObject> JsonObject, bool& bOutSuccess, FString& OutInfoMessage);
+private:
+	
+};
