e4b8b4e414e9140399052f2194c9f2011c08cc98
Sam Thompson <samthompson73@live.co.uk>
2023-10-30 12:22:33 +0000

    PM-49: Setup main menu

---
+++ Source/ProjectMars/UI/Widgets/MainMenuWidget.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenuWidget.h"
+
+UMainMenuWidget::UMainMenuWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UMainMenuWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
---
+++ Source/ProjectMars/UI/Widgets/MainMenuWidget.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARS_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	explicit UMainMenuWidget(const FObjectInitializer& ObjectInitializer);
+	
+	virtual void NativeConstruct() override;
+
+	virtual void NativeOnInitialized() override;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UButton* StartButton{ nullptr };
+
+private:
+	
+};
